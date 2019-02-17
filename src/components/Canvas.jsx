66012690649584f16021d78f4e3da2fc3cfcf90f
star-citizen <Christoph07@gmail.com>
2019-02-17 02:41:43 -0500

    Added moving the mouse

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,27 +4,37 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
 
 const Canvas = (props) => {
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-    return (
-        <svg
-            id="aliens-go-home-canvas"
-            preserveAspectRatio="xMaxYMax none"
-            onMouseMove={props.trackMouse}
-            viewBox={viewBox}
-        >
-            <Sky />
-            <Ground />
-            <CannonPipe rotation={props.angle} />
-            <CannonBase />
-      </svg>
-    )
+	const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+	return (
+		<svg
+			id="aliens-go-home-canvas"
+			preserveAspectRatio="xMaxYMax none"
+			onMouseMove={props.trackMouse}
+			viewBox={viewBox}
+		>
+		  <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+			<Sky />
+			<Ground />
+			<CannonPipe rotation={props.angle} />
+			<CannonBase />
+			<CurrentScore score={15} />
+			<FlyingObject position={{x: -150, y: -300}}/>
+      <FlyingObject position={{x: 150, y: -300}}/>
+		</svg>
+	)
 };
 
 Canvas.propTypes = {
-    angle: PropTypes.number.isRequired,
-    trackMouse: PropTypes.func.isRequired,
+	angle: PropTypes.number.isRequired,
+	trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
